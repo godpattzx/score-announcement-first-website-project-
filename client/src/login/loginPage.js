01f0c16f68e9cf4d61fbf5cfe03f63f552d40280
พชรพล ศุกลสกุล <patt502090@hotmail.com>
2023-12-31 23:41:42 +0700

    login_page (lasted version)

--- client/src/login/loginPage.js
+++ client/src/login/loginPage.js
@@ -27,7 +27,7 @@ const LoginForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-   // setSubmitEnabled(false);
+   setSubmitEnabled(false);
     setLoading(true);
     await delay(500);
 
@@ -37,7 +37,8 @@ const LoginForm = () => {
         password: password,
       });
       axiosConfig.jwt = result.data.jwt;
-      setShow(true);
+      setSubmitEnabled(true);
+      
 
       result = await axios.get(
         "http://localhost:1337/api/users/me?populate=role"
@@ -53,8 +54,15 @@ const LoginForm = () => {
     } catch (e) {
       console.log(e);
       console.log("wrong username & password");
-     // setSubmitEnabled(true);
+      
+      setShow(true);
+    
     }
+    finally {
+      
+      setLoading(false);
+    }
+    
   
   };
   const handlePassword = () => {};
@@ -64,82 +72,83 @@ const LoginForm = () => {
   }
 
   return (
+    
     <div
-      className="sign-in__wrapper"
-      style={{ backgroundImage: `url(${BackgroundImage})` }}
-    >
-      {/* Overlay */}
-      <div className="sign-in__backdrop"></div>
-      {/* Form */}
-      <Form className="shadow p-4 bg-white rounded" onSubmit={handleSubmit}>
-        {/* Header */}
-        <img
-          className="img-thumbnail mx-auto d-block mb-2"
-          src={Logo}
-          alt="logo"
+    className="sign-in__wrapper"
+    style={{ backgroundImage: `url(${BackgroundImage})` }}
+  >
+    {/* Overlay */}
+    <div className="sign-in__backdrop"></div>
+    {/* Form */}
+    <Form className="shadow p-4 bg-white rounded" onSubmit={handleSubmit}>
+      {/* Header */}
+      <img
+        className="img-thumbnail mx-auto d-block mb-2"
+        src={Logo}
+        alt="logo"
+      />
+      <div className="h4 mb-2 text-center">Sign In</div>
+      {/* ALert */}
+      {show ? (
+        <Alert
+          className="mb-2"
+          variant="danger"
+          onClose={() => setShow(false)}
+          dismissible
+        >
+          Incorrect username or password.
+        </Alert>
+      ) : (
+        <div />
+      )}
+      <Form.Group className="mb-2" controlId="username">
+        <Form.Label>Username</Form.Label>
+        <Form.Control
+          type="text"
+          value={username}
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+          required
         />
-        <div className="h4 mb-2 text-center">Sign In</div>
-        {/* ALert */}
-        {show ? (
-          <Alert
-            className="mb-2"
-            variant="danger"
-            onClose={() => setShow(false)}
-            dismissible
-          >
-            Incorrect username or password.
-          </Alert>
-        ) : (
-          <div />
-        )}
-        <Form.Group className="mb-2" controlId="username">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            value={username}
-            placeholder="Username"
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-2" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-2" controlId="checkbox">
-          <Form.Check type="checkbox" label="Remember me" />
-        </Form.Group>
-        {!loading ? (
-          <Button className="w-100" variant="primary" type="submit">
-            Log In
-          </Button>
-        ) : (
-          <Button className="w-100" variant="primary" type="submit" disabled>
-            Logging In...
-          </Button>
-        )}
-        <div className="d-grid justify-content-end">
-          <Button
-            className="text-muted px-0"
-            variant="link"
-            onClick={handlePassword}
-          >
-            Forgot password?
-          </Button>
-        </div>
-      </Form>
-      {/* Footer */}
-      <div className="w-100 mb-2 position-absolute bottom-0 start-50 translate-middle-x text-white text-center">
-        pann project | &copy;2022
+      </Form.Group>
+      <Form.Group className="mb-2" controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </Form.Group>
+      <Form.Group className="mb-2" controlId="checkbox">
+        <Form.Check type="checkbox" label="Remember me" />
+      </Form.Group>
+      {!loading ? (
+        <Button className="w-100" variant="primary" type="submit">
+          Log In
+        </Button>
+      ) : (
+        <Button className="w-100" variant="primary" type="submit" disabled>
+          Logging In...
+        </Button>
+      )}
+      <div className="d-grid justify-content-end">
+        <Button
+          className="text-muted px-0"
+          variant="link"
+          onClick={handlePassword}
+        >
+          Forgot password?
+        </Button>
       </div>
+    </Form>
+    {/* Footer */}
+    <div className="w-100 mb-2 position-absolute bottom-0 start-50 translate-middle-x text-white text-center">
+      Made by Hendrik C | &copy;2022
     </div>
-  );
+  </div>
+);
 };
 
 export default LoginForm;
